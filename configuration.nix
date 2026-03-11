79b7c170356a70c681140d29b87bd295242c5c83
AibaKo <ratroniii@gmail.com>
2026-03-11 21:19:37 +0600

    Comment out amdgpu kernel module
    Comment out the amdgpu kernel module in the configuration.

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  # boot.initrd.kernelModules = [ "amdgpu" ];
 
   # Kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
