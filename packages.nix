f2a21ed405d59687b6559313119cba16513230e7
AibaKo <ratroniii@gmail.com>
2026-04-04 20:39:36 +0000

    Clean up packages.nix by removing comments
    Removed commented sections for clarity.

--- packages.nix
+++ packages.nix
@@ -6,19 +6,10 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
-    # Desktop apps
     vlc
-
-    # KDE
-
-    # CLI utils
-
-    # Language runtimes & toolchains
-
-    # Code editor
   ];
   
   fonts.packages = with pkgs; [
-    times-newer-roman
+
   ];
 }
