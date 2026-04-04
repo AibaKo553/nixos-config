25bf7d76509e75d2d80f733e36750d1d5d15bcb9
AibaKo <ratroniii@gmail.com>
2026-04-04 20:19:43 +0000

    Clean up systemPackages in packages.nix
    Removed several desktop applications and CLI utilities from systemPackages in packages.nix.

--- packages.nix
+++ packages.nix
@@ -8,53 +8,18 @@
   environment.systemPackages = with pkgs; [
     # Desktop apps
     chromium
-    telegram-desktop
-    gimp
-    discord
-    winbox4
-    media-downloader
-    libreoffice-qt
-    hunspell
-    hunspellDicts.ru_RU
-    qbittorrent
-    cpu-x
     vlc
-    droidcam
-    mangohud
-    element-desktop
-    prismlauncher
 
     # KDE
-    kdePackages.kdenlive
 
     # CLI utils
-    wget
-    tree
-    htop
-    mcrcon
-    docker-compose
-    mc
 
     # Language runtimes & toolchains
-    # python314
-    # virtualenv
-    # jdk
 
-    # # Code editor
-    # jetbrains.pycharm-oss
-    # jetbrains.idea-oss
-    # vscode.fhs
+    # Code editor
   ];
   
   fonts.packages = with pkgs; [
     times-newer-roman
   ];
-
-  virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "aiba" ];
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.dragAndDrop = true;
-
-  programs.kdeconnect.enable = true;
 }
